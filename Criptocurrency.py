d9f69d5bf6550a612bc118394f799e1416204384
abhishekgupta23 <53884462+abhishekgupta23@users.noreply.github.com>
2021-05-13 11:57:17 +0530

    Add files via upload

---
+++ Criptocurrency.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import requests
+import pandas
+from bs4 import BeautifulSoup
+def app():
+    st.title('CRIPTO CURRENCY DETAILS')
+    st.write('Details related to CRIPTO CURRENCY')
+    def get_url():
+        domain='https://gadgets.ndtv.com/bitcoin-price-today-india-inr-usd-compare-koinex-zebpay-price-litecoin-iota'
+        return domain
+    def fetchdata(url):
+        try:
+            data=requests.get(url)
+            return data
+        except Exception as e:
+            print("some error")
+            print(e)
+    def parseData(text):
+        try:
+            soup=BeautifulSoup(text,features='html.parser')
+            return soup
+        except Exception as e:
+            print('error while parsing')
+            print(e)
+    def u(n,p1):
+        for j in p1[0:n]:
+            x=j
+        return x
+    def l(f,q):
+        for j in q[0:f]:
+            x=j
+        return x
+    def n(f,r):
+        for j in r[0:f]:
+            x=j
+        return x
+    def h(f,c):
+        for j in c[0:f]:
+            x=j.text
+        return x   
+    def world_news():
+        soup=None 
+        try:
+            data=fetchdata(get_url())
+            if data.status_code==200:
+                soup=parseData(data.text)
+        except Exception as e:
+            print(e)
+        st.write(soup.find('h1').text)
+        st.write(soup.find('h2').text)
+        a=soup.find_all('div',attrs={'class':'_flx _cpnm'})
+        b=soup.find_all('td',attrs={'class':'_lft'})
+        c=soup.find_all('span',attrs={'class':'_rtxt'})
+        d=[]
+        for i in b:
+            details = {}
+            details =i.text
+            d.append(details)
+        p=[]
+        for t in d[::3]:
+            w={}
+            w=t
+            p.append(w)
+        q=[]
+        for m in d[1::3]:
+            v={}
+            v=m
+            q.append(v)
+        r=[]
+        for s in d[2::3]:
+            t={}
+            t=s
+            r.append(t)
+        covid_data=[]
+        k=1
+        for j in a:
+            data={}
+            z=u(k,p)
+            y=l(k,q)
+            o=n(k,r)
+            v=h(k,c)
+            k=k+1
+            data['coin name(code)'] =j.text
+            data['price']=z
+            data['Change (24h)'] = v
+            data['Market Cap'] = y
+            data['Volume (24h)'] = o
+            covid_data.append(data)
+        df = pandas.DataFrame(covid_data)
+        
+        st.table(df)
+        if st.button('Download'):
+            df.to_csv('WORLDNEW_DATA.csv')
+    world_news()
