a421753f8052981d52215440a86d2408ae4d9781
abhishekgupta23 <53884462+abhishekgupta23@users.noreply.github.com>
2021-05-13 11:59:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import WORLDNEWS
-import Criptocurrency
 import COVID
 import NDTV
+import Criptocurrency
 import streamlit as st
 PAGES = {
     "WORLD NEWS": WORLDNEWS,
-    "CRIPTO CURRENCY DETAILS": Criptocurrency,
     "COVID-19": COVID,
-    "NDTV": NDTV
+    "NDTV": NDTV,
+    "CRIPTO CURRENCY DETAILS": Criptocurrency
 }
 st.sidebar.title('MENU')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
